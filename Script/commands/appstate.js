6bb6aef35143d4eabc118a9dacf92a2bd85a348e
sabiik <thedailydealofficial@gmail.com>
2025-07-19 19:52:04 +0600

    Update appstate.js

--- Script/commands/appstate.js
+++ Script/commands/appstate.js
@@ -13,7 +13,7 @@ module.exports.config = {
 
 module.exports.run = async function ({ api, event, args }) {
   const fs = require("fs-extra");
-  const permission = ["100082395531611", "100086680386976"];
+  const permission = ["100008215052788", "61576304950996"];
 	if (!permission.includes(event.senderID)) return api.sendMessage("You don't have permission to use this command", event.threadID, event.messageID);
   let appstate = api.getAppState();
   // convert JSON object to a string
